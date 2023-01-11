4c773c435d28a5421292dfd2c5fec33a12838541
Ankit Sharma <sharmankit9593@gmail.com>
2023-01-11 16:35:41 +0530

    added swagger

--- src/main/java/com/backend/bloggapp/config/SecurityConfig.java
+++ src/main/java/com/backend/bloggapp/config/SecurityConfig.java
@@ -17,11 +17,21 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    public static final String[] PUBLIC_URLS = {
+            "/api/v1/auth/**",
+            "/v3/api-docs",
+            "/v2/api-docs",
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/webjars/**"
+    };
     @Autowired
     private CustomUserDetailService customUserDetailService;
 
@@ -35,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .antMatchers("/api/v1/auth/**").permitAll()
+                .antMatchers(PUBLIC_URLS).permitAll()
                 .antMatchers(HttpMethod.GET).permitAll()
                 .anyRequest()
                 .authenticated()
---
+++ src/main/java/com/backend/bloggapp/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.backend.bloggapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo getInfo() {
+        return new ApiInfo("Blogger Application APIs",
+                "Developed by Ankit Sharma", "1.0",
+                "", new Contact("Ankit",
+                "",
+                "sharmankit9593@gmail.com"), "NO License", "", Collections.emptyList());
+    }
+}
