1a3659bacfa23ab954c3d0d0c5a57113cac45d11
Ankit Sharma <sharmankit9593@gmail.com>
2023-01-13 13:58:01 +0530

    finished swagger configuration

--- src/main/java/com/backend/bloggapp/config/SwaggerConfig.java
+++ src/main/java/com/backend/bloggapp/config/SwaggerConfig.java
@@ -4,19 +4,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    private ApiKey apiKeys () {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private List<SecurityContext> securityContexts () {
+        return Arrays.asList(SecurityContext.builder().securityReferences(securityReferences()).build());
+    }
+
+    private List<SecurityReference> securityReferences () {
+        AuthorizationScope scopes = new AuthorizationScope("global", "Access Everything");
+        return Arrays.asList(new SecurityReference("JWT", new AuthorizationScope[]{scopes}));
+    }
     @Bean
     public Docket api() {
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(getInfo())
+                .securityContexts(securityContexts())
+                .securitySchemes(Arrays.asList(apiKeys()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
